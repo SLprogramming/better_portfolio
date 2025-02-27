1483d82a96a5a9648d7ff3ea8f772eea15d674e8
Taing Linn Maung <joesat2516@gmail.com>
2025-02-27 14:04:23 +0630

    change sttatic data

--- src/helper/portfolios.js
+++ src/helper/portfolios.js
@@ -2,9 +2,11 @@
 
 export const portfolioData = [
     { id: 1, title: "App UI Design", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/app-1.jpg" },
-    { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "Product", image: "/assets/img/portfolio/app-1.jpg" },
-    { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Branding", image: "/assets/img/portfolio/app-1.jpg" },
-    { id: 5, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/app-1.jpg" },
+    { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "Product", image: "/assets/img/portfolio/app-2.jpg" },
+    { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Branding", image: "/assets/img/portfolio/app-3.jpg" },
+    { id: 4, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Branding", image: "/assets/img/portfolio/books-1.jpg" },
+    { id: 5, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/books-2.jpg" },
+    { id: 6, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/product-1.jpg" },
    
   ];
   
