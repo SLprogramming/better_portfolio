852755c6e4cb2b4df22704bfbe99f937255ccb3e
Taing Linn Maung <joesat2516@gmail.com>
2025-04-07 13:56:03 +0630

    test

--- src/helper/portfolios.js
+++ src/helper/portfolios.js
@@ -5,6 +5,7 @@ export const portfolioData = [
     { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "Product", image: "/assets/img/portfolio/app-1.jpg" },
     { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Branding", image: "/assets/img/portfolio/app-1.jpg" },
     { id: 4, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/app-1.jpg" },
+    { id: 5, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/app-1.jpg" },
    
   ];
   
