7ed364f7fd8d199b299961190b5aec2d3a82f097
Taing Linn Maung <joesat2516@gmail.com>
2025-04-08 15:27:30 +0630

    add popup for image portfolio

--- src/helper/portfolios.js
+++ src/helper/portfolios.js
@@ -1,7 +1,7 @@
 
 
 export const portfolioData = [
-    { id: 1, title: "App UI Design", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_1.jpg" },
+    { id: 1, title: "App UI Design", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_1.jpg" , link:'https://www.youtube.com/' },
     { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/website_2.jpg" },
     { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Logo", image: "/assets/img/portfolio/website_3.jpg" },
     { id: 4, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "logo", image: "/assets/img/portfolio/logos.png" },
