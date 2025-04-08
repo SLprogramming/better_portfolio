80032811806a1b6a090e3ea357f1d980829965ee
Su Pyae <65438577+thaeoosupyaesone@users.noreply.github.com>
2025-04-08 16:22:41 +0630

    add:link

--- src/helper/portfolios.js
+++ src/helper/portfolios.js
@@ -1,9 +1,9 @@
 
 
 export const portfolioData = [
-    { id: 1, title: "App UI Design", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_1.jpg" , link:'https://www.youtube.com/' },
-    { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_2.jpg" },
-    { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_3.jpg" },
+    { id: 1, title: "App UI Design", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_1.jpg" , link:'https://www.exploremyanmar.com/' },
+    { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_2.jpg" , link:'https://www.goldenexpresstours.com/' },
+    { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_3.jpg" , link:'https://www.goldeneagleballooning.com/' },
     { id: 4, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "Logo", image: "/assets/img/portfolio/logos.png" },
     { id: 5, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/app-1.jpg" },
    
