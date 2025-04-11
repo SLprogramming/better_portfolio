538f9c3173ac2da4504b44a25a578710a6a90ac4
Su Pyae <65438577+thaeoosupyaesone@users.noreply.github.com>
2025-04-11 10:38:16 +0630

    change:title

--- src/helper/portfolios.js
+++ src/helper/portfolios.js
@@ -1,11 +1,11 @@
 
 
 export const portfolioData = [
-    { id: 1, title: "App UI Design", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_1.jpg" , link:'https://www.exploremyanmar.com/' },
-    { id: 2, title: "Smart Watch", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_2.jpg" , link:'https://www.goldenexpresstours.com/' },
-    { id: 3, title: "Cosmetics Branding", description:'Lorem ipsum, dolor sit amet consectetur',category: "Website", image: "/assets/img/portfolio/website_3.jpg" , link:'https://www.goldeneagleballooning.com/' },
-    { id: 4, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "Logo", image: "/assets/img/portfolio/logos.png" },
-    { id: 5, title: "Mobile Wallet", description:'Lorem ipsum, dolor sit amet consectetur',category: "App", image: "/assets/img/portfolio/app-1.jpg" },
+    { id: 1, title: "App UI Design", description:'Your Website is the window of your bussiness..',category: "Website", image: "/assets/img/portfolio/website_1.jpg" , link:'https://www.exploremyanmar.com/' },
+    { id: 2, title: "Smart Watch", description:'Your Website is the window of your bussiness..',category: "Website", image: "/assets/img/portfolio/website_2.jpg" , link:'https://www.goldenexpresstours.com/' },
+    { id: 3, title: "Cosmetics Branding", description:'Your Website is the window of your bussiness..',category: "Website", image: "/assets/img/portfolio/website_3.jpg" , link:'https://www.goldeneagleballooning.com/' },
+    { id: 4, title: "Mobile Wallet", description:'A logo is primary function is not to sell directly.. ',category: "Logo", image: "/assets/img/portfolio/logos.png" },
+    { id: 5, title: "Mobile Wallet", description:'Design is so simple, That is why It is so complicated',category: "App", image: "/assets/img/portfolio/app.jpg" },
    
   ];
   
